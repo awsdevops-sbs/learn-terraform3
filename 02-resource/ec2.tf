b755521c06db16961c6d0198bce31608c3101367
akram <akram.khan200@gmail.com>
2026-01-03 19:06:45 +0530

    terraform example 2

--- 02-resource/ec2.tf
+++ 02-resource/ec2.tf
@@ -6,14 +6,37 @@ terraform {
     }
   }
 }
-resource "aws_instance" "test1" {
+resource "aws_instance" "frontend_terraform" {
 
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.medium"
   vpc_security_group_ids = ["sg-0262c4232ab2a8184"]
 
   tags = {
-    Name = "test_VM3"
+    Name = "frontend_terraform"
+
+  }
+}
+
+resource "aws_instance" "backend_terraform" {
+
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-0262c4232ab2a8184"]
+
+  tags = {
+    Name = "backend_terraform"
+
+  }
+}
+resource "aws_instance" "mysql_terraform" {
+
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-0262c4232ab2a8184"]
+
+  tags = {
+    Name = "mysql_terraform"
 
   }
 }
