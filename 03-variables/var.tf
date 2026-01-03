527d98ddd56ab980822ecdb18b6e0f05cf65f54c
akram <akram.khan200@gmail.com>
2026-01-03 19:16:28 +0530

    terraform example 3

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -6,3 +6,26 @@ variable "x" {
 output "x" {
   value = var.x
 }
+
+variable "list" {
+
+  default = [10,20,30]
+}
+
+output "list" {
+  value = var.list
+}
+
+variable "map" {
+  default = {
+
+    a=10,
+    b=20,
+    c=30
+  }
+
+}
+
+output "map" {
+  value = var.map
+}
