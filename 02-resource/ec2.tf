d7ebc5838a2d12b2d2a1bd7e0d7fde442ccb8b6c
akram <akram.khan200@gmail.com>
2026-01-03 18:49:16 +0530

    terraform example 2

--- 02-resource/ec2.tf
+++ 02-resource/ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "test" {
 
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.medium"
-  vpc_security_group_ids = ["vpc-0d2850636350f0540"]
+  vpc_security_group_ids = ["sg-0262c4232ab2a8184"]
 
   tags = {
     name = "test VM1"
