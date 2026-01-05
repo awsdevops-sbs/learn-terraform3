ac13a454f6e3a8cf21b4746595d9103db701a47d
akram <akram.khan200@gmail.com>
2026-01-05 18:08:22 +0530

    terraform example 7

--- 07-Loops/main.tf
+++ 07-Loops/main.tf
@@ -10,3 +10,21 @@ resource "null_resource" "test" {
 
   count =10
 }
+
+resource "null_resource" "test2" {
+
+  for_each = var.colors
+}
+
+variable "colors" {
+
+  default = {
+
+     "31" = "red"
+    "32" = "yellow"
+    "33" = "purple"
+    "34" = "white"
+
+  }
+
+}
