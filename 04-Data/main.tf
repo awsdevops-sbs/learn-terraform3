2e1c4829de94cb2dc495aa22cb7394a87c180106
akram <akram.khan200@gmail.com>
2026-01-05 12:14:50 +0530

    terraform example 3

--- 04-Data/main.tf
+++ 04-Data/main.tf
@@ -1,9 +1,9 @@
-variable "security_group" {}
+
 
 data "aws_security_group" "selected" {
   name = "Allow-all-from-public"
 }
 
 output "security_group" {
-  value = var.aws_security_group.selected
+  value = data.aws_security_group.selected
 }
